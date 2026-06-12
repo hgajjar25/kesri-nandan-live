d69fa64008c75667fab7f09cb64a027fcd26f376
Harshil Gajjar <113493385+hgajjar25@users.noreply.github.com>
2026-06-12 11:31:41 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, collection, getDocs, setDoc, doc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBThcFm8T5oYWDgH1UfLkNm1HQXdMSQen0",
@@ -12,4 +12,14 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+export async function loadData(collectionName) {
+  const snapshot = await getDocs(collection(db, collectionName));
+  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+}
+
+export async function saveData(collectionName, id, data) {
+  await setDoc(doc(db, collectionName, id), data);
+}
+
 export default app;
